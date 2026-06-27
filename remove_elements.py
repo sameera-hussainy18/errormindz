e21898713a39d2cca67eb4117c93453b20d1baed
Pattan Sameera Hussainy <sammupattan.0915@gmail.com>
2026-06-27 22:46:41 +0530

    Create remove_elements.py

---
+++ remove_elements.py
@@ -0,0 +1,16 @@
+def removeElement(nums, val):
+    k = 0
+
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[k] = nums[i]
+            k += 1
+
+    return k
+nums = [3, 2, 2, 3]
+val = 3
+
+k = removeElement(nums, val)
+
+print("k =", k)
+print("nums =", nums[:k])
